5a89d613f8b1087bdb9a735391432167ef1ba2ab
yehch <10856004@ntub.edu.tw>
2024-12-06 14:32:42 +0800

    Nice巴底的N

--- nicebodyweb/static/components/layout/footer.js
+++ nicebodyweb/static/components/layout/footer.js
@@ -2,7 +2,7 @@ let renderFooter = function() {
   let footerStr = `
   <footer>
     <img src="/static/images/logo.jpg" alt="">
-    <p>&copy;2024 nice巴底</p>
+    <p>&copy;2024 Nice巴底</p>
   </footer>
   `;
 
